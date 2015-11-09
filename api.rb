0e21b91d66199b74020fe36760ab8e0011a36803
Kohei MATSUSHITA <ma2shita+git@ma2shita.jp>
2015-11-09 22:36:44 +0900

    DRYize by namespace feature

--- api.rb
+++ api.rb
@@ -15,32 +15,34 @@ class WorkingDashboard::API < Grape::API
   version :v1, using: :path
   format :json
 
-  params do
-    requires :iid,        type: String,                     desc: "Identify Key (Unique on System)"
-    optional :status,     type: String, default: "updated", desc: "IID's latest status"
-    optional :annotation, type: JSON,                       desc: "Annotate data"
-  end
-  post "/statuses" do # Create shared with Update (PUT does not implement)
-    d_params = declared(params)
-    r = Helper::ItemModel.upsert(d_params["iid"], d_params["status"], d_params["annotation"])
-    if r == :updated
-      status 200
+  namespace :statuses do
+    params do
+      requires :iid,        type: String,                     desc: "Identify Key (Unique on System)"
+      optional :status,     type: String, default: "updated", desc: "IID's latest status"
+      optional :annotation, type: JSON,                       desc: "Annotate data"
     end
-    {response: r}
-  end
+    post do # Create shared with Update (PUT does not implement)
+      d_params = declared(params)
+      r = Helper::ItemModel.upsert(d_params["iid"], d_params["status"], d_params["annotation"])
+      if r == :updated
+        status 200
+      end
+      {response: r}
+    end
 
-  get "/statuses" do
-    r = Item.all
-    present r, with: ::API::Entities::Status
-  end
+    get do
+      r = Item.all
+      present r, with: ::API::Entities::Status
+    end
 
-  params do
-    requires :iid, type: String, desc: "Identify Key (Unique on System)"
-  end
-  delete "/statuses" do
-    d_params = declared(params)
-    Item.where(iid: d_params["iid"]).delete
-    status 202
-    {response: :accepted}
+    params do
+      requires :iid, type: String, desc: "Identify Key (Unique on System)"
+    end
+    delete do
+      d_params = declared(params)
+      Item.where(iid: d_params["iid"]).delete
+      status 202
+      {response: :accepted}
+    end
   end
 end
