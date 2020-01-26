25d24deb71018b999f10dc229394b41f0546a327
Kohei MATSUSHITA <ma2shita+git@ma2shita.jp>
2020-01-26 19:39:40 +0900

    Remove '/statuses'

--- api.rb
+++ api.rb
@@ -20,41 +20,37 @@ class FrightBoard::API < Grape::API
   end
 
   namespace ':board_id' do
-    namespace :statuses do
+    params do
+      requires :board_id,   type: String,                     desc: "Unique in system"
+      requires :iid,        type: String,                     desc: "Identify key (Unique in board)"
+      optional :status,     type: String, default: "updated", desc: "IID's latest status"
+      optional :annotation, type: JSON,                       desc: "Annotate data"
+    end
+    post do # Create shared with Update (PUT does not implement)
+      d_params = declared(params)
+      r = Helper::ItemModel.upsert(d_params["board_id"], d_params["iid"], d_params["status"], d_params["annotation"])
+      status 200 if r == :updated
+      {response: r}
+    end
 
-      params do
-        requires :board_id,   type: String,                     desc: "Unique in system"
-        requires :iid,        type: String,                     desc: "Identify key (Unique in board)"
-        optional :status,     type: String, default: "updated", desc: "IID's latest status"
-        optional :annotation, type: JSON,                       desc: "Annotate data"
-      end
-      post do # Create shared with Update (PUT does not implement)
-        d_params = declared(params)
-        r = Helper::ItemModel.upsert(d_params["board_id"], d_params["iid"], d_params["status"], d_params["annotation"])
-        status 200 if r == :updated
-        {response: r}
-      end
+    params do
+      requires :board_id, type: String, desc: "Unique in system"
+    end
+    get do
+      d_params = declared(params)
+      r = Item.where(board_id: d_params["board_id"]).all
+      present r, with: API::Entities::Status
+    end
 
-      params do
-        requires :board_id, type: String, desc: "Unique in system"
-      end
-      get do
-        d_params = declared(params)
-        r = Item.where(board_id: d_params["board_id"]).all
-        present r, with: API::Entities::Status
-      end
-
-      params do
-        requires :board_id, type: String, desc: "Unique in system"
-        requires :iid,      type: String, desc: "Identify Key (Unique on System)"
-      end
-      delete do
-        d_params = declared(params)
-        Item.where(board_id: d_params["board_id"], iid: d_params["iid"]).delete
-        status 202
-        {response: :accepted}
-      end
-
+    params do
+      requires :board_id, type: String, desc: "Unique in system"
+      requires :iid,      type: String, desc: "Identify Key (Unique on System)"
+    end
+    delete do
+      d_params = declared(params)
+      Item.where(board_id: d_params["board_id"], iid: d_params["iid"]).delete
+      status 202
+      {response: :accepted}
     end
   end
 end
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -3,23 +3,23 @@ require "spec_helper"
 describe FrightBoard::API do
   def app ; described_class ; end # required
 
-  describe "/statuses" do
+  describe "/:BOARD_ID1" do
     context "not exists" do
       before {
         Board.truncate
         Item.truncate
       }
-      subject { post("/api/v1/b1/statuses", {iid:"i1"}, {}) }
+      subject { post("/api/v1/BOARD_ID1", {iid:"i1"}, {}) }
       its(:status) { should be 404 }
     end
 
     context "create" do
       before {
         Board.truncate
-        Board.create(board_id: "b1")
+        Board.create(board_id: "BOARD_ID1")
         Item.truncate
       }
-      subject { post("/api/v1/b1/statuses", {iid:"i1"}, {}) }
+      subject { post("/api/v1/BOARD_ID1", {iid:"i1"}, {}) }
       its(:status) { should be 201 }
       let(:response) do
         {response: "created"}
@@ -30,12 +30,12 @@ describe FrightBoard::API do
     context "update" do
       before {
         Board.truncate
-        Board.create(board_id: "b1")
-        Board.create(board_id: "b2")
+        Board.create(board_id: "BOARD_ID1")
+        Board.create(board_id: "BOARD_ID2")
         Item.truncate
-        Helper::ItemModel.upsert("b1", "i1")
+        Helper::ItemModel.upsert("BOARD_ID1", "i1")
       }
-      subject { post("/api/v1/b1/statuses", {iid:"i1"}, {}) }
+      subject { post("/api/v1/BOARD_ID1", {iid:"i1"}, {}) }
       its(:status) { should be 200 }
       let(:response) do
         {response: "updated"}
@@ -46,25 +46,26 @@ describe FrightBoard::API do
     context "get" do
       let(:list) do
         [
-          {board_id: "b1", iid: "XXXX1", status: "donwloading",     annotation: {mac_address:"AA:BB:CC:00:11:01"}.to_json},
-          {board_id: "b1", iid: "XXXX2", status: "donwloading",     annotation: {mac_address:"AA:BB:CC:00:11:02"}.to_json},
-          {board_id: "b1", iid: "XXXX1", status: "running_ansible", annotation: {mac_address:"AA:BB:CC:00:11:01"}.to_json}, # update
-          {board_id: "b2", iid: "XXXX1", status: "void"},
+          {board_id: "BOARD_ID1", iid: "XXXX1", status: "donwloading",     annotation: {mac_address:"AA:BB:CC:00:11:01"}.to_json},
+          {board_id: "BOARD_ID1", iid: "XXXX2", status: "donwloading",     annotation: {mac_address:"AA:BB:CC:00:11:02"}.to_json},
+          {board_id: "BOARD_ID1", iid: "XXXX1", status: "running_ansible", annotation: {mac_address:"AA:BB:CC:00:11:01"}.to_json}, # update
+          {board_id: "BOARD_ID2", iid: "XXXX1", status: "void"},
         ]
       end
       before {
         Board.truncate
-        Board.create(board_id: "b1")
+        Board.create(board_id: "BOARD_ID1")
+        Board.create(board_id: "BOARD_ID2")
         Item.truncate
         list.each do |i|
           Helper::ItemModel.upsert(*i.values)
         end
       }
-      subject { get("/api/v1/b1/statuses") }
+      subject { get("/api/v1/BOARD_ID1") }
       let(:response) do
         [
-          {board_id: "b1", iid: "XXXX1", status: "running_ansible", annotation: {mac_address:"AA:BB:CC:00:11:01"}.to_json, created_at: String, updated_at: String},
-          {board_id: "b1", iid: "XXXX2", status: "donwloading",     annotation: {mac_address:"AA:BB:CC:00:11:02"}.to_json, created_at: String, updated_at: String},
+          {board_id: "BOARD_ID1", iid: "XXXX1", status: "running_ansible", annotation: {mac_address:"AA:BB:CC:00:11:01"}.to_json, created_at: String, updated_at: String},
+          {board_id: "BOARD_ID1", iid: "XXXX2", status: "donwloading",     annotation: {mac_address:"AA:BB:CC:00:11:02"}.to_json, created_at: String, updated_at: String},
         ]
       end
       its(:body) { should be_json_as response }
@@ -73,22 +74,22 @@ describe FrightBoard::API do
     context "delete" do
       let(:list) do
         [
-          {board_id: "b1", iid: "XXXX1"},
-          {board_id: "b1", iid: "XXXX2"},
-          {board_id: "b1", iid: "XXXX3"},
-          {board_id: "b2", iid: "XXXX4"},
+          {board_id: "BOARD_ID1", iid: "XXXX1"},
+          {board_id: "BOARD_ID1", iid: "XXXX2"},
+          {board_id: "BOARD_ID1", iid: "XXXX3"},
+          {board_id: "BOARD_ID2", iid: "XXXX4"},
         ]
       end
       before {
         Board.truncate
-        Board.create(board_id: "b1")
-        Board.create(board_id: "b2")
+        Board.create(board_id: "BOARD_ID1")
+        Board.create(board_id: "BOARD_ID2")
         Item.truncate
         list.each do |i|
           Helper::ItemModel.upsert(*i.values)
         end
       }
-      subject { delete("/api/v1/b1/statuses", {iid:"XXXX2"}, {}) }
+      subject { delete("/api/v1/BOARD_ID1", {iid:"XXXX2"}, {}) }
       its(:status) { should be 202 }
       let(:response) do
         {response: "accepted"}
